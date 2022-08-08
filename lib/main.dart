fedc878f8e8aab09d80599550cd1b7d37a635eab
beyzaevcen <beyzaevcen37@icloud.com>
2022-08-08 22:37:43 +0300

    update commit

--- lib/main.dart
+++ lib/main.dart
@@ -25,8 +25,11 @@ class QuizPage extends StatefulWidget {
 }
 
 class _QuizPageState extends State<QuizPage> {
-  List<Icon> scorekeeper =[
-
+  List<Icon> scorekeeper =[];
+  List<String> questionkeeper =[
+    'Beyza\'s favorite color is green.',
+    'Beyza\'s favorite tv series is The Walking Dead',
+    'Beyza is not fearless.',
   ];
   @override
   Widget build(BuildContext context) {
@@ -40,7 +43,7 @@ class _QuizPageState extends State<QuizPage> {
             padding: EdgeInsets.all(10.0),
             child: Center(
               child: Text(
-                'This is where the question text will go.',
+                'This is where the question   will go.',
                 textAlign: TextAlign.center,
                 style: TextStyle(
                   fontSize: 25.0,
@@ -110,7 +113,7 @@ class _QuizPageState extends State<QuizPage> {
 }
 
 /*
-question1: 'You can lead a cow down stairs but not up stairs.', false,
-question2: 'Approximately one quarter of human bones are in the feet.', true,
-question3: 'A slug\'s blood is green.', true,
+ 'Beyza\'s favorite color is green.',
+ 'Beyza\'s favorite tv series is The Walking Dead',
+ 'Beyza is not fearless.',
 */
